146785b9c22e6b9f9744cf8dc3212a2e9f284741
Nick Millward <nicholasmillward@gmail.com>
2018-06-20 09:26:38 -0400

    initial commit

---
+++ app/src/main/java/com/nicholasmillward/usgsearthquakes/QuakesActivity.java
@@ -0,0 +1,13 @@
+package com.nicholasmillward.usgsearthquakes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class QuakesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quakes);
+    }
+}
