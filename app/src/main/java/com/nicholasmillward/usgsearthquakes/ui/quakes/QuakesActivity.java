b15592ca41cdbde698b89548566d04ae11277b10
Nick Millward <nicholasmillward@gmail.com>
2018-06-20 10:08:33 -0400

    refactor package structure - added data and ui

--- app/src/main/java/com/nicholasmillward/usgsearthquakes/ui/quakes/QuakesActivity.java
+++ app/src/main/java/com/nicholasmillward/usgsearthquakes/ui/quakes/QuakesActivity.java
@@ -1,7 +1,9 @@
-package com.nicholasmillward.usgsearthquakes;
+package com.nicholasmillward.usgsearthquakes.ui.quakes;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.nicholasmillward.usgsearthquakes.R;
 
 public class QuakesActivity extends AppCompatActivity {
 
