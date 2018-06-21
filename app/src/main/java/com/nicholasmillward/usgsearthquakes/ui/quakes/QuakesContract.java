27862bfca79c0ab753c44e5f3c3b8f8b367c6f08
Nick Millward <nicholasmillward@gmail.com>
2018-06-21 08:05:06 -0400

    setup basic MVP framework

---
+++ app/src/main/java/com/nicholasmillward/usgsearthquakes/ui/quakes/QuakesContract.java
@@ -0,0 +1,17 @@
+package com.nicholasmillward.usgsearthquakes.ui.quakes;
+
+/**
+ * Created by nmillward on 6/21/18.
+ */
+
+public interface QuakesContract {
+
+    interface View {
+
+    }
+
+    interface Presenter {
+
+    }
+
+}
---
+++ app/src/main/java/com/nicholasmillward/usgsearthquakes/ui/quakes/QuakesPresenter.java
@@ -0,0 +1,8 @@
+package com.nicholasmillward.usgsearthquakes.ui.quakes;
+
+/**
+ * Created by nmillward on 6/21/18.
+ */
+
+public class QuakesPresenter {
+}
