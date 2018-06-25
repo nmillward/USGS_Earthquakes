ef970b801f6d02bfb39b83b917c05b54a3379ca5
Nick Millward <nicholasmillward@gmail.com>
2018-06-25 08:39:16 -0400

    network null check

--- app/src/main/java/com/nicholasmillward/usgsearthquakes/utils/NetworkUtils.java
+++ app/src/main/java/com/nicholasmillward/usgsearthquakes/utils/NetworkUtils.java
@@ -13,6 +13,7 @@ public class NetworkUtils {
     public static boolean isNetworkAvailable(Context context) {
 
         ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        assert connectivityManager != null;
         NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
         return (activeNetworkInfo != null) && activeNetworkInfo.isConnectedOrConnecting();
 
